8470ff467037e67f547d74d1136945079e06754f
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-09-12 12:54:16 +0800

    basic: Showing data in SwiftUI

--- Gusto/ContentView.swift
+++ Gusto/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
 
     @Environment(\.modelContext) var modelContext
 
+    @Query var restaurants: [Restaurant]
+
     let names: [String] = [
         "Wok this Way",
         "Thyme Square",
@@ -21,10 +23,20 @@ struct ContentView: View {
     ]
 
     var body: some View {
-        EmptyView()
-            .onAppear {
-                createRestaurants(with: names)
+        NavigationStack {
+            List {
+                ForEach(restaurants, id: \.self) {
+                    Text(verbatim: $0.name)
+                }
             }
+        }
+        Button(action: {
+            createRestaurants(with: names)
+        }, label: {
+            // an Image with the SFSymbol named "plus.circle"
+            Image(systemName: "plus.circle")
+                .foregroundColor(.blue)
+        })
     }
 }
 
--- Gusto/GustoApp.swift
+++ Gusto/GustoApp.swift
@@ -13,6 +13,6 @@ struct GustoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-        }
+        }.modelContainer(for: Restaurant.self)
     }
 }
