4aa0cde7c5a19dd8079589aecb52e6e05412d665
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-09-12 12:46:20 +0800

    basic: Creating data

--- Gusto/ContentView.swift
+++ Gusto/ContentView.swift
@@ -10,8 +10,40 @@ import SwiftData
 
 struct ContentView: View {
 
+    @Environment(\.modelContext) var modelContext
+
+    let names: [String] = [
+        "Wok this Way",
+        "Thyme Square",
+        "Pasta la Vista",
+        "Life of Pie",
+        "Lord of the Wings"
+    ]
+
     var body: some View {
         EmptyView()
+            .onAppear {
+                createRestaurants(with: names)
+            }
+    }
+}
+
+// MARK: Private functions
+extension ContentView {
+
+    private func createRestaurants(with names: [String]) {
+        names
+            .map { name in
+                Restaurant(
+                    name: name,
+                    priceRating: 300,
+                    qualityRating: 1,
+                    speedRating: 1
+                )
+            }
+            .forEach {
+                modelContext.insert($0)
+            }
     }
 }
 
