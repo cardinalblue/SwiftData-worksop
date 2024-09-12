3f0f835406ce8dbef925748f6b0bd5f64c57f1e0
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-09-12 12:38:33 +0800

    Initial commit

---
+++ Gusto/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Gusto
+//
+//  Created by Chiaote Ni on 2024/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+        EmptyView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gusto/GustoApp.swift
@@ -0,0 +1,18 @@
+//
+//  GustoApp.swift
+//  Gusto
+//
+//  Created by Chiaote Ni on 2024/3/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GustoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
