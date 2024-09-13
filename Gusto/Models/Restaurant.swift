2c2674e6c5f95a2d7bad4e591538e341388a84b1
Chiaote Ni <aaron.chiao.19@gmail.com>
2024-09-13 10:04:57 +0800

    fix: temporarily remove the unique key before implementing migration

--- Gusto/Models/Restaurant.swift
+++ Gusto/Models/Restaurant.swift
@@ -14,11 +14,13 @@ class Restaurant {
 
     // Erase All Content And Settings to clear the old data.
     // It's a breaking changes for the local DB when set up a unique, which we didn't set up for the existing DB data
-    @Attribute(.unique) var name: String // It’s not a good practice to use name as the unique key, but let’s go with it for now since it’s just demo code for practice purposes.
+//    @Attribute(.unique) var name: String // It’s not a good practice to use name as the unique key, but let’s go with it for now since it’s just demo code for practice purposes.
     /*
      You should be very careful when using @Attribute(.unique) with any properties,
      because it is not supported when you want to store your user’s data in iCloud (which is most of the time!)
      */
+
+    var name: String
 
     var priceRating: Int
     var qualityRating: Int
